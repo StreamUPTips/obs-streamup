06c58019b261e1b3b57af094451940263d49e266
Exeldro <exelsor@hotmail.com>
2021-08-24 22:19:05 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# StreamUP for OBS Studio
+
+Plugin for OBS Studio to add StreamUP to the tools menu.
+
+
+# Build
+- Build OBS Studio: https://obsproject.com/wiki/Install-Instructions
+- Check out this repository to UI/frontend-plugins/streamup
+- Add `add_subdirectory(streamup)` to UI/frontend-plugins/CMakeLists.txt
+- Rebuild OBS Studio
+
+# Donations
+https://www.paypal.me/exeldro
---
+++ streamup.cpp
@@ -0,0 +1,152 @@
+#include "streamup.hpp"
+#include <obs-module.h>
+#include <QFileDialog>
+#include "version.h"
+
+#define QT_UTF8(str) QString::fromUtf8(str)
+#define QT_TO_UTF8(str) str.toUtf8().constData()
+
+OBS_DECLARE_MODULE()
+OBS_MODULE_AUTHOR("Exeldro");
+OBS_MODULE_USE_DEFAULT_LOCALE("streamup", "en-US")
+
+void ResizeMoveSetting(obs_data_t *obs_data, float factor)
+{
+	double x = obs_data_get_double(obs_data, "x");
+	obs_data_set_double(obs_data, "x", x * factor);
+	double y = obs_data_get_double(obs_data, "y");
+	obs_data_set_double(obs_data, "y", y * factor);
+	obs_data_release(obs_data);
+}
+
+void ResizeMoveFilters(obs_source_t *parent, obs_source_t *child, void *param)
+{
+	float factor = *((float *)param);
+	if (strcmp(obs_source_get_unversioned_id(child),
+		   "move_source_filter") != 0)
+		return;
+	obs_data_t *settings = obs_source_get_settings(child);
+	ResizeMoveSetting(obs_data_get_obj(settings, "pos"), factor);
+	ResizeMoveSetting(obs_data_get_obj(settings, "scale"), factor);
+	ResizeMoveSetting(obs_data_get_obj(settings, "bounds"), factor);
+	obs_data_set_string(settings, "transform_text", "");
+	obs_data_release(settings);
+	return;
+}
+
+void ResizeSceneItems(obs_data_t *settings, float factor)
+{
+	obs_data_array_t *items = obs_data_get_array(settings, "items");
+	size_t count = obs_data_array_count(items);
+
+	for (size_t i = 0; i < count; i++) {
+		obs_data_t *item_data = obs_data_array_item(items, i);
+		vec2 vec2;
+		obs_data_get_vec2(item_data, "pos", &vec2);
+		vec2.x *= factor;
+		vec2.y *= factor;
+		obs_data_set_vec2(item_data, "pos", &vec2);
+		obs_data_get_vec2(item_data, "scale", &vec2);
+		vec2.x *= factor;
+		vec2.y *= factor;
+		obs_data_set_vec2(item_data, "scale", &vec2);
+		obs_data_get_vec2(item_data, "bounds", &vec2);
+		vec2.x *= factor;
+		vec2.y *= factor;
+		obs_data_set_vec2(item_data, "bounds", &vec2);
+		obs_data_release(item_data);
+	}
+	obs_data_array_release(items);
+}
+
+static void LoadSources(obs_data_array_t *data)
+{
+	const size_t count = obs_data_array_count(data);
+	std::vector<obs_source_t *> sources;
+	sources.reserve(count);
+	uint32_t w = obs_source_get_width(obs_frontend_get_current_scene());
+	float factor = (float)w / 1920.0f;
+	for (size_t i = 0; i < count; i++) {
+		obs_data_t *sourceData = obs_data_array_item(data, i);
+		const char *name = obs_data_get_string(sourceData, "name");
+
+		bool new_source = true;
+		obs_source_t *s = obs_get_source_by_name(name);
+		if (!s)
+			s = obs_load_source(sourceData);
+		else
+			new_source = false;
+		if (s)
+			sources.push_back(s);
+		bool resize = false;
+		obs_scene_t *scene = obs_scene_from_source(s);
+		if (!scene)
+			scene = obs_group_from_source(s);
+		else
+			resize = true;
+		if (scene) {
+			obs_data_t *scene_settings =
+				obs_data_get_obj(sourceData, "settings");
+			if (resize && w != 1920) {
+				ResizeSceneItems(scene_settings, factor);
+				if (new_source)
+					obs_source_enum_filters(
+						s, ResizeMoveFilters, &factor);
+			}
+
+			obs_source_update(s, scene_settings);
+			obs_data_release(scene_settings);
+		}
+		obs_data_release(sourceData);
+	}
+
+	for (obs_source_t *source : sources)
+		obs_source_load(source);
+
+	for (obs_source_t *source : sources)
+		obs_source_release(source);
+}
+
+static void LoadScene(obs_data_t *data)
+{
+	if (!data)
+		return;
+	obs_data_array_t *sourcesData = obs_data_get_array(data, "sources");
+	if (!sourcesData)
+		return;
+	LoadSources(sourcesData);
+	obs_data_array_release(sourcesData);
+}
+
+void LoadStreamUpFile(void *private_data)
+{
+	QString fileName = QFileDialog::getOpenFileName(
+		nullptr, QT_UTF8(obs_module_text("Load")), QString(),
+		"StreamUP File (*.streamup)");
+	if (fileName.isEmpty())
+		return;
+	obs_data_t *data = obs_data_create_from_json_file(QT_TO_UTF8(fileName));
+	LoadScene(data);
+	obs_data_release(data);
+}
+
+bool obs_module_load()
+{
+	blog(LOG_INFO, "[StreamUP] loaded version %s", PROJECT_VERSION);
+
+	obs_frontend_add_tools_menu_item(obs_module_text("StreamUp"),
+					 LoadStreamUpFile, nullptr);
+	return true;
+}
+
+void obs_module_unload() {}
+
+MODULE_EXPORT const char *obs_module_description(void)
+{
+	return obs_module_text("Description");
+}
+
+MODULE_EXPORT const char *obs_module_name(void)
+{
+	return obs_module_text("StreamUP");
+}
