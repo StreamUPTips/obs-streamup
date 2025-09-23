e0ff63bd10bfa94a14f38a62ef3d265c88080771
Andilippi <saintcoolio@hotmail.co.uk>
2025-09-23 12:11:21 +0100

    build fix

--- .github/scripts/Build-Windows.ps1
+++ .github/scripts/Build-Windows.ps1
@@ -47,7 +47,7 @@ function Build {
     Push-Location -Stack BuildTemp
     Ensure-Location $ProjectRoot
 
-    $CmakeArgs = @('--preset', "windows-ci-${Target}")
+    $CmakeArgs = @('--preset', "windows-${Target}-ci")
     $CmakeBuildArgs = @('--build')
     $CmakeInstallArgs = @()
 
